c0541a528cf48ad5e63a9b45d80e8ddc91975748
hxbdy <hxbdy541@gmail.com>
2022-11-12 11:58:48 +0900

    RankOneHotClassログ変更

--- src/deepLearning/encoding/encoder/Encoder_RankOneHot.py
+++ src/deepLearning/encoding/encoder/Encoder_RankOneHot.py
@@ -41,14 +41,14 @@ class RankOneHotClass(XClass):
             for i in range(adj_size):
                 del self.xList[-1]
 
-            # 1位の馬がリストから無くなってしまった。
-            # 残ってる馬の中で一番順位がよかった馬を正解とする
-            if (1 in self.xList) == False:
-                logger.info("1st place horse was deleted, https://db.netkeiba.com/race/{0}/".format(self.race_id))
-
     def nrm(self):
         # 最小値の順位を取得
         rank_min = min(self.xList)
+
+        if rank_min != 1:
+            # pad処理により1位の馬がリストから無くなってしまった。
+            # 残っている馬の中で一番順位がよかった馬を正解とする
+            logger.info("Fastest rank : 1 -> {0} | https://db.netkeiba.com/race/{1}/".format(rank_min, self.race_id))
 
         # 最高順位を1とする
         for i in range(len(self.xList)):
