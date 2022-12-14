88d4e3d572764388da8769a93f1522ce5b366d98
hxbdy <hxbdy541@gmail.com>
2022-12-15 01:01:49 +0900

    完成形コメント追加

--- all.bat
+++ all.bat
@@ -1,5 +1,14 @@
+REM update DB
+REM ###
+
 REM scrape
 python ./src/scraping/netkeiba_scraping.py %1 %2
+
+REM normalize
+REM ###
+
+REM deep learning
+REM ###
 
 REM predict
 python ./src/deepLearning/nn/predict.py
