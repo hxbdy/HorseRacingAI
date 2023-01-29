bba99119e360ae5b44387b7df5555625c4023531
hxbdy <hxbdy541@gmail.com>
2023-01-29 20:59:13 +0900

    レース、馬のスクレイピング毎に進行状況を保存する

--- src/scraping/netkeiba_scraping.py
+++ src/scraping/netkeiba_scraping.py
@@ -6,6 +6,8 @@ import re
 import argparse
 import pickle
 from dateutil.relativedelta import relativedelta
+from collections import deque
+import os
 
 from selenium.webdriver.common.by import By
 
@@ -60,7 +62,7 @@ def create_table():
     jockey_infoテーブル: 騎手の騎乗回数を1年ごとに計上してまとめたテーブル
     """
 
-    dbname = config.get("common", "path_netkeibaDB")
+    dbname = path_ini("common", "path_netkeibaDB")
     conn = sqlite3.connect(dbname)
     cur = conn.cursor()
     cur.execute('CREATE TABLE race_id(race_No INTEGER PRIMARY KEY AUTOINCREMENT, id TEXT)')
@@ -697,27 +699,67 @@ def update_database_learning(driver, start_YYMM, end_YYMM, race_grade="-1"):
     race_grade: 取得するグレードのリスト 1: G1, 2: G2, 3: G3, 4: OP以上全て, -1: G1&G2&G3
     """
 
-    # 期間内のrace_idを取得してrace_idテーブルへ保存
-    if race_grade== "-1":
-        for i in [1, 2, 3]:
-            scrape_raceID(driver, start_YYMM, end_YYMM, str(i))
+    path_untracked_raceid = "./src/scraping/untracked_raceid.pickle"
+
+    # 途中でクラッシュした場合は True にセットする
+    # 未調査のraceidのpickleファイルを読み込んでそこから再開する
+    RESUME_FLG = False
+
+    # =================================================================
+
+    # 未調査のraceidを用意する
+    if RESUME_FLG == True:
+        # load
+        with open(path_untracked_raceid, 'rb') as f:
+            stack:deque = pickle.load(f)
     else:
-        scrape_raceID(driver, start_YYMM, end_YYMM, race_grade)
+        # 期間内のrace_idを取得してrace_idテーブルへ保存
+        # このブロックはクラッシュしない前提
+        # ここでクラッシュした場合、RESUME_FLG = False でやり直しが必要
+        ##############################################################
+        if race_grade== "-1":
+            for i in [1, 2, 3]:
+                scrape_raceID(driver, start_YYMM, end_YYMM, str(i))
+        else:
+            scrape_raceID(driver, start_YYMM, end_YYMM, race_grade)
+        ##############################################################
+
+        # 未調査のrace_idのリストを作成
+        raceID_list = make_raceID_list()
+        stack = deque(raceID_list)
+
+    logger.info("scrape race_id count = {0}".format(len(stack)))
     
-    # 未調査のrace_idのリストを作成
-    raceID_list = make_raceID_list()
-    # レース結果をスクレイプしてrace_resultテーブルへ保存。レースで出走した馬のリストを得る。
-    horseID_list = scrape_racedata(driver, raceID_list)
+    # =================================================================
 
-    # 馬のリストから重複とhorse idが数字になっていない馬を削除
-    horseID_list = list(set(horseID_list))
-    horseID_list_cleaned = []
-    for horseID in horseID_list:
-        if horseID.isdecimal():
-            horseID_list_cleaned.append(horseID)
+    while len(stack)!=0:
+        race_id = stack.pop()
+        logger.info("update_database_learning() race_id = {0}".format(race_id))
 
-    # 馬の情報をスクレイプしてhorse_profテーブルとrace_infoテーブルへ保存
-    scrape_horsedata(driver, horseID_list_cleaned)
+        # race_id のレース情報をスクレイピングしてrace_resultテーブルのアップデート
+        # +
+        # 出走馬のスクレイピング
+        horseID_list = scrape_racedata(driver, [race_id])
+
+        # 馬のリストから重複とhorse idが数字になっていない馬を削除
+        horseID_list = list(set(horseID_list))
+        horseID_list_cleaned = []
+        for horseID in horseID_list:
+            if horseID.isdecimal():
+                horseID_list_cleaned.append(horseID)
+
+        # 馬の情報をスクレイプしてhorse_profテーブルとrace_infoテーブルへ保存
+        scrape_horsedata(driver, horseID_list_cleaned)
+
+        # save
+        with open(path_untracked_raceid, 'wb') as f:
+            pickle.dump(stack, f)
+
+    # delete stack
+    if os.path.isfile(path_untracked_raceid):
+        os.remove(path_untracked_raceid)
+
+    # =================================================================
 
     # 騎手の騎乗回数を更新
     head_year = datetime.datetime.strptime(start_YYMM, '%Y%m').year
