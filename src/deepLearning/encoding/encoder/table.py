a5457b0dd56e311665ed501fea42d7a4defe8927
hxbdy <hxbdy541@gmail.com>
2023-04-23 12:30:46 +0900

    BradleyTerryClass以外無効

--- src/deepLearning/encoding/encoder/table.py
+++ src/deepLearning/encoding/encoder/table.py
@@ -2,27 +2,27 @@ import encoder
 
 # 学習時の入力用テーブル
 XTbl = [
-    encoder.Encoder_Money.MoneyClass,
-    encoder.Encoder_HorseNum.HorseNumClass,
-    encoder.Encoder_CourseCondition.CourseConditionClass,
-    encoder.Encoder_CourseDistance.CourseDistanceClass,
-    encoder.Encoder_RaceStartTime.RaceStartTimeClass,
-    encoder.Encoder_Weather.WeatherClass,
-    encoder.Encoder_HorseAge.HorseAgeClass,
-    encoder.Encoder_BurdenWeight.BurdenWeightClass,
-    encoder.Encoder_PostPosition.PostPositionClass,
-    encoder.Encoder_Jockey.JockeyClass,
-    encoder.Encoder_CumPerform.CumPerformClass,
+    # encoder.Encoder_Money.MoneyClass,
+    # encoder.Encoder_HorseNum.HorseNumClass,
+    # encoder.Encoder_CourseCondition.CourseConditionClass,
+    # encoder.Encoder_CourseDistance.CourseDistanceClass,
+    # encoder.Encoder_RaceStartTime.RaceStartTimeClass,
+    # encoder.Encoder_Weather.WeatherClass,
+    # encoder.Encoder_HorseAge.HorseAgeClass,
+    # encoder.Encoder_BurdenWeight.BurdenWeightClass,
+    # encoder.Encoder_PostPosition.PostPositionClass,
+    # encoder.Encoder_Jockey.JockeyClass,
+    # encoder.Encoder_CumPerform.CumPerformClass,
     encoder.Encoder_BradleyTerry.BradleyTerryClass,
-    encoder.Encoder_Umamusume.UmamusumeClass,
-    encoder.Encoder_FatherBradleyTerry.FatherBradleyTerryClass,
-    encoder.Encoder_MotherBradleyTerry.MotherBradleyTerryClass,
-    encoder.Encoder_Last3f.Last3fClass,
-    encoder.Encoder_HorseWeight.HorseWeightClass,
-    encoder.Encoder_CornerPos.CornerPosClass,
-    encoder.Encoder_Pace.PaceClass,
-    encoder.Encoder_Review.ReviewClass,
-    encoder.Encoder_LastRaceLeft.LastRaceLeftClass
+    # encoder.Encoder_Umamusume.UmamusumeClass,
+    # encoder.Encoder_FatherBradleyTerry.FatherBradleyTerryClass,
+    # encoder.Encoder_MotherBradleyTerry.MotherBradleyTerryClass,
+    # encoder.Encoder_Last3f.Last3fClass,
+    # encoder.Encoder_HorseWeight.HorseWeightClass,
+    # encoder.Encoder_CornerPos.CornerPosClass,
+    # encoder.Encoder_Pace.PaceClass,
+    # encoder.Encoder_Review.ReviewClass,
+    # encoder.Encoder_LastRaceLeft.LastRaceLeftClass
 ]
 
 # 正解用テーブル
